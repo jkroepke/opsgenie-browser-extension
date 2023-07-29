0aab4189a7c8a442f588c0d56a9d58aade0c4142
Jan-Otto Kröpke <mail@jkroepke.de>
2023-07-29 19:09:52 +0200

    fix unsafe method

--- chrome/js/popup.js
+++ chrome/js/popup.js
@@ -22,72 +22,120 @@ document.addEventListener('DOMContentLoaded', () => {
     await renderAlerts()
 })()
 
+function sendMessage(e) {
+    e.preventDefault();
+
+    chrome.runtime.sendMessage({
+        action: e.target.dataset.action,
+        id: e.target.dataset.id,
+    }, (error) => {
+        if (error) {
+            window.alert(error)
+        }
+    })
+}
+
+function createElement(tagName, text, className) {
+    const e = document.createElement(tagName)
+    e.textContent = text
+    if (className !== undefined) {
+        e.classList.add(className)
+    }
+    return e;
+}
+
+function createAlertActionElement(alert, settings) {
+    const tdAlertAction = document.createElement("td")
+    tdAlertAction.classList.add('alert-action')
+
+    if (settings.username) {
+        let a
+
+        if (alert.acknowledged) {
+            a = createElement('a', '[CLOSE]', 'handleAlert');
+            a.dataset.action = 'close'
+        } else {
+            a = createElement('a', '[ACK]', 'handleAlert');
+            a.dataset.action = 'ack'
+        }
+
+        a.dataset.id = alert.id
+        a.addEventListener('click', sendMessage);
+        tdAlertAction.appendChild(a)
+    }
+    return tdAlertAction;
+}
+
+function createAlertPriorityElement(alert) {
+    const tdAlertPriority = document.createElement("td")
+    const spanAlertPriority = createElement("span", alert.priority, "alert-priority")
+    spanAlertPriority.classList.add(`${alert.priority}-bg`)
+    tdAlertPriority.appendChild(spanAlertPriority)
+    return tdAlertPriority;
+}
+
+function createAlertMessageElement(alert, settings) {
+    const tdAlertMessage = createElement("td", alert.message, 'alert-message')
+    tdAlertMessage.addEventListener('click', e => {
+        e.preventDefault()
+
+        let url = "https://"
+        if (settings.customerName !== '') {
+            url += `${settings.customerName}.`
+        }
+        url += `app.opsgenie.com/alert/detail/${e.target.parentElement.id}/details`
+
+        window.open(url, '_blank')
+    });
+    return tdAlertMessage;
+}
+
 async function renderAlerts() {
     const elemAlerts = document.getElementById("alerts")
     const elemInfo = document.getElementById("result")
     const settings = await chrome.storage.sync.get()
     const {popup} = await chrome.storage.session.get('popup')
-    elemAlerts.innerHTML = ''
+    elemAlerts.textContent = elemInfo.textContent = ''
 
     if (!popup) {
-        elemInfo.innerHTML = `<p style="color:#BF2600">Loading ...</p>`
+        elemInfo.appendChild(createElement('p', 'Loading ...', 'warning'))
         return
     }
 
     if (!popup.ok) {
-        elemInfo.innerHTML = `<p style="color:#BF2600">${popup.data}</p>`
+        elemInfo.appendChild(createElement('p', popup.data, 'warning'))
         return
     }
 
-    elemInfo.innerHTML = `<i>Last updated @ ${popup.time}</i><a style="float: right;" href="${popup.ogUrl}" target="_blank"> see all alerts↗</a>`
+    const lastUpdate = createElement("i", `Last updated @ ${popup.time}`)
+    elemInfo.appendChild(lastUpdate)
+
+    const allAlertsLink = createElement("a", ` see all alerts↗`, 'right')
+    allAlertsLink.setAttribute('href', popup.ogUrl)
+    allAlertsLink.setAttribute('target', '_blank')
+    elemInfo.appendChild(allAlertsLink)
+
     if (popup.data.length === 0) {
-        elemInfo.innerHTML += "<p style=\"text-align:center\"> There are no alerts.  🎉</p>"
-        return
+        elemAlerts.appendChild(createElement("p", ' There are no alerts.  🎉', 'center'))
     }
 
     popup.data.forEach(alert => {
-        let alertRow = `<tr class="alert" id="${alert.id}"><td class="alert-action">`
-        if (settings.username) {
-            if (alert.acknowledged) {
-                alertRow += `<a href="#" class="handleAlert" data-action="close" data-id="${alert.id}">[CLOSE]</a>`
-            } else {
-                alertRow += `<a href="#" class="handleAlert" data-action="acknowledge" data-id="${alert.id}">[ACK]</a>`
-            }
-        }
-        alertRow += `</td><td class="alert-count">x${alert.count}</td><td><span class="alert-priority ${alert.priority}-bg">${alert.priority}</span></td><td class="alert-message">${alert.message}</td></tr>`
+        const tr = document.createElement("tr")
+        tr.setAttribute('id', alert.id)
+        tr.classList.add('alert')
 
-        elemAlerts.insertAdjacentHTML('beforeend', alertRow)
-    })
+        const tdAlertAction = createAlertActionElement(alert, settings);
+        tr.appendChild(tdAlertAction);
 
-    if (settings.username) {
-        [...(document.getElementsByClassName('handleAlert'))].forEach(d => {
-            d.addEventListener('click', (e) => {
-                e.preventDefault();
+        const tdAlertCount = createElement("td", `x${alert.count}`, 'alert-count')
+        tr.appendChild(tdAlertCount);
 
-                chrome.runtime.sendMessage({
-                    action: e.target.dataset.action,
-                    id: e.target.dataset.id,
-                }, (error) => {
-                    if (error) {
-                        window.alert(error)
-                    }
-                })
-            });
-        })
-    }
+        const tdAlertPriority = createAlertPriorityElement(alert);
+        tr.appendChild(tdAlertPriority);
 
+        const tdAlertMessage = createAlertMessageElement(alert, settings);
+        tr.appendChild(tdAlertMessage);
 
-    [...(document.getElementsByClassName('alert-message'))].forEach(d => {
-        d.addEventListener('click', (e) => {
-            e.preventDefault()
-
-            let url = "https://"
-            if (settings.customerName !== '') {
-                url += `${settings.customerName}.`
-            }
-            url += `app.opsgenie.com/alert/detail/${e.target.parentElement.id}/details`
-
-            window.open(url, '_blank')
-        });
-    })
+        elemAlerts.appendChild(tr)
+    });
 }
